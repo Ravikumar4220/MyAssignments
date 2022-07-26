b79afb89899cb4c020215c35ce2621791c53da57
Ravikumar4220 <109679152+Ravikumar4220@users.noreply.github.com>
2022-07-26 18:49:36 +0530

    Add files via upload
    Week2.Day2

---
+++ day2/TenthTable.java
@@ -0,0 +1,14 @@
+package week2.day2;
+
+public class TenthTable {
+	
+	    public static void main(String[] args) {
+		
+		int a;
+		a=10;
+		for (int i = 1; i <=10; i++) {	
+		int table=a*i;	
+		System.out.println(i+"*"+a+"="+table);
+		}
+   	   }
+      }
